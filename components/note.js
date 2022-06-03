82b5a042bde86e337f94956c57c741423ef94fc0
mariley <mariley2018@gmail.com>
2022-06-02 21:42:09 -0500

    integrate action for add todo item

--- components/note.js
+++ components/note.js
@@ -1,15 +1,32 @@
-const note = new Vue({
-    el: "#note",
-    data:{
-        notecontent: '',
-        activo: true
+const app = new Vue({
+  el: "#todo-list",
+  data: {
+    todoNew: {
+      text: 'New',
+      activo: true,
+      id: null,
     },
-    methods:{
-        estado()
-        {
-            activo:false,
-            console.log("click")
-            
-        }
+    activo: true,
+
+    todoList: []
+  },
+  methods: {
+    estado () {
+      activo: false,
+        console.log("click")
+
+    },
+    addTodoItem () {
+      const itemId = this.todoList.length
+      const itemData = {
+        text: this.todoNew.text,
+        activo: true,
+        id: itemId,
+      }
+
+      this.todoList.push(itemData)
+
+      console.log(this.todoList)
     }
+  }
 })
