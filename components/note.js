a97e76811681d0f78a812365ac59156b86605f53
DANIELNAHUN <nahuncf5@gmail.com>
2022-07-24 18:20:43 -0500

    Fixing misspellings

--- components/note.js
+++ components/note.js
@@ -3,58 +3,46 @@ const app = new Vue({
   data: {
     todoNew: {
       text: '',
-      stateitem: true,
+      state: true,
       id: null,
     },
-    activo: true,
-
     todoList: []
   },
   methods: {
-    estado () {
-      activo: false,
-        console.log("click")
-
-    },
     addTodoItem () {
       const itemId = this.todoList.length
       const itemData = {
         text: this.todoNew.text || "Add text",
-        stateitem: true,
+        state: true,
         id: itemId,
       }
-
       this.todoList.push(itemData)
-
-      console.log(this.todoList)
     },
     deleteTodoItem (id) {
       const itemId = id
       if (itemId > -1) {
         this.todoList.splice(itemId, 1);
       };
-      this.todoNew.text = ""      
+      this.todoNew.text = ""
     },
-    editTodoItem (id){
+    editTodoItem (id) {
       const itemId = id
       const textList = this.todoList[itemId].text
-      this.todoList.splice(itemId, 1,{
+      this.todoList.splice(itemId, 1, {
         text: textList,
-        estadoitem: false,
+        state: false,
         id: id,
       });
     },
-    confirmTodoItem(item){
-
+    confirmTodoItem (item) {
       const itemId = item.id
       const itemData = {
         text: item.text,
-        stateitem: true,
+        state: true,
         id: itemId,
       }
-      this.todoList.splice(itemId,1,itemData)
+      this.todoList.splice(itemId, 1, itemData)
       this.todoNew.text = ""
-
-    }    
+    }
   }
 })
